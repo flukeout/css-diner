b2f3c1c6708b38d78a3eb55a350d7c9a93196de4
Luke Pacholski <luke.pacholski@gmail.com>
2014-02-09 20:17:27 -0800

    Blam

--- levels.js
+++ levels.js
@@ -15,14 +15,14 @@ var levels = [
     doThis : "Clear the plates",
     selector : "plate",
     helpTitle : "Tag Name Selector",
-    help : "Use the tag name to get all elements of that type.<br/> Ex. &rarr; <strong>div</strong> will select all <strong>&lt;div&gt;</strong> elements",
+    help : "Use the tag name to select elements of that type.<br/> Ex. &rarr; <strong>div</strong> will select all <strong>&lt;div&gt;</strong> elements",
     board: "()()"
   },
   {
     doThis : "Clear the bentos",
     selector : "bento",
     helpTitle : "Tag Name Selector",
-    help : "Use the tag name to get all elements of that type.<br/> Ex. &rarr; <strong>div</strong> will select all <strong>&lt;div&gt;</strong> elements",
+    help : "Use the tag name to select all elements of that type.<br/> Ex. &rarr; <strong>div</strong> will select all <strong>&lt;div&gt;</strong> elements",
     board: "[]()[]"
   },
   {
