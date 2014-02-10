79a23e0db2fb5905ce3e99f07db600c351f17d80
Luke Pacholski <luke.pacholski@gmail.com>
2014-02-10 11:53:44 -0800

    Level 5 fix

--- levels.js
+++ levels.js
@@ -40,7 +40,7 @@ var levels = [
     board: "[](A)A"
   },
   {
-    doThis : "Select the pickle on the plate",
+    doThis : "Select the pickle on the fancy plate",
     selector : "#fancy pickle",
     helpTitle: "Combine the Descendant Selector",
     help : 'You can use any selector inside of a descendent selector. <br> Ex &rarr; <strong>#cool&nbsp;&nbsp;A</strong> will get all <strong>A</strong> elements that are inside of the element with <strong>id="cool"</strong>',
@@ -74,7 +74,6 @@ var levels = [
     help : 'Thanks to Shatner technology, we can combine selectors with commas.<br> Ex &rarr; <strong>p , .fun</strong> will select all <strong>&lt;p&gt;</strong> elements as well as all elements with <strong>class="fun"</strong> ',
     board: "pP(P)[P](P)Pp"
   },
-
   {
     doThis : "Select all the things!",
     selector : "*",
@@ -82,98 +81,11 @@ var levels = [
     help : 'You can select everything with the <strong>*</strong> selector! <br> You can also use &rarr; <strong>p *</strong> to select everything inside of all <strong>&lt;p&gt;</strong> tags.',
     board: "A(o)[][a][O]{)"
   },
-
-  // {
-  //   doThis : "Clear the bentos!",
-  //   selector : "bento",
-  //   help : "Use the <strong>element</strong> selector to select all elements of that type!",
-  //   board: "simple"
-  // },
-  // {
-  //   doThis : "Clear all the things!",
-  //   selector : "*",
-  //   help : "Use the <strong>*</strong> selector to select all elements!",
-  //   board: "simple"
-  // },
-  // {
-  //   doThis : "Clear all the things!",
-  //   selector : "*",
-  //   help : "Use the <strong>*</strong> selector to select all elements!",
-  //   board: "simple"
-  // },
-  // {
-  //   doThis : "Eat the Big Apple (tm)!",
-  //   selector : ".apple.big",
-  //   help : "Combine two or more <strong>class</strong> selectors!",
-  // },
-  // {
-  //   doThis : "Clear the orange on the table and the empty plate!",
-  //   selector : ":not(:empty) + :empty",
-  //   help : "Combine the <strong>:not</strong> and <strong>:empty</strong> selector!"
-  // },
-  // {
-  //   doThis : "Eat the orange not on a plate!",
-  //   selector : ".orange:not(:first-child)",
-  //   help : "Combine the <strong>:not</strong> and <strong>:empty</strong> selector!"
-  // },
-  // {
-  //   doThis : "Clear all the full servings!",
-  //   selector : ":not(:empty)",
-  //   help : "Combine the <strong>:not</strong> and <strong>:empty</strong> selector!"
-  // },
-  // {
-  //   doThis : "Clear all the plates!",
-  //   selector : ".plate",
-  //   help : "Use the <strong>.class</strong> selector to select all elements with that class!"
-  // },
-  //
-  // {
-  //   doThis : "Clear the fancy plate!",
-  //   selector : "#fancy",
-  //   help : "Use the <strong>id</strong> selector to select an element with that id!"
-  // },
-  // {
-  //   doThis : "Eat the apple on the plate!",
-  //   selector : ".plate .apple",
-  //   help : "Select the apple using the <strong>descendant</strong> selector!"
-  // },
-  // {
-  //   doThis : "Clean all the apples and pickles!",
-  //   selector : ".apple, .pickle",
-  //   help : "Use two <strong>class</strong> selectors at the same time <strong>comma<strong>."
-  // },
-  // {
-  //   doThis : "Clear every bento that has a plate somewhere to it's left!",
-  //   selector : ".plate ~ .bento",
-  //   help : "Use the <strong>preceded</strong> selector to all select elements that are preceded by another element."
-  // },
-  // {
-  //   doThis : "Clean each plate that has a bento on the left",
-  //   selector : ".bento + .plate",
-  //   help : "Use the <strong>+</strong> selector to select all elements that are immediately preceded by another element."
-  // },
-  // {
-  //   doThis : "Clean all the servings!",
-  //   selector : ".serving",
-  // },
-  // {
-  //   doThis : "Clean the 3rd serving!",
-  //   selector : ".serving:nth-child(3)",
-  // },
-  // {
-  //   doThis : "Clean the first serving!",
-  //   selector : ".serving:first-child",
-  // },
-  // {
-  //   doThis : "Clean the empty plate!",
-  //   selector : ".plate:empty",
-  // },
-  // {
-  //   doThis : "Clean the last serving!",
-  //   selector : ".plate:last-child",
-  // },
-  // {
-  //   doThis : "Clean the last serving!",
-  //   selector : ".serving:last-child",
-  // }
+  {
+    doThis : "Select everything on a plate",
+    selector : "plate *",
+    helpTitle: "The All Selector",
+    help : 'You can select everything with the <strong>*</strong> selector! <br> You can also use &rarr; <strong>p *</strong> to select everything inside of all <strong>&lt;p&gt;</strong> tags.',
+    board: "{o)(P)a(A)"
+  }
 ];
